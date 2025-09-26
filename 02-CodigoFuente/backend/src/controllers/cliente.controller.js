a0500268885bebacc8c7281ea968dc758e888f20
Hide17298 <arnoldelgadocalle5@gmail.com>
2025-09-26 17:13:21 -0400

    feat(controller): implementar endpoint createCliente @Arnol

--- 02-CodigoFuente/backend/src/controllers/cliente.controller.js
+++ 02-CodigoFuente/backend/src/controllers/cliente.controller.js
@@ -9,3 +9,13 @@ export const getClientes = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+export const createCliente = async (req, res) => {
+    try {
+        const { nombre, direccion, celular, email, estado } = req.body;
+        const cliente = await Cliente.create({ nombre, direccion, celular, email, estado });
+        res.status(201).json(cliente);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
