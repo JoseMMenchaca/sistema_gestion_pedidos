8a457bdb4fec2aaff80b51c9b31749787169d170
Hide17298 <arnoldelgadocalle5@gmail.com>
2025-10-06 11:26:48 -0400

    refactor(routes): Simplificar lógica en rutas de cliente

--- 02-CodigoFuente/backend/src/routes/cliente.routes.js
+++ 02-CodigoFuente/backend/src/routes/cliente.routes.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
-import { createCliente, 
-         getClientes, 
-         getCliente, 
-         updateCliente, 
-         getClientePedidos } from "../controllers/cliente.controller.js";
+import { 
+    createCliente, 
+    getClientes, 
+    getCliente, 
+    updateCliente 
+} from "../controllers/cliente.controller.js";
 
 const router = Router();
 
-router.get("/clientes", getClientes);
-router.post("/clientes", createCliente);
-router.get("/clientes/:id", getCliente);
-router.put("/clientes/:id", updateCliente);
-router.get("/clientes/:id/pedidos", getClientePedidos);
+router.get("/", getClientes);
+router.post("/", createCliente);
+router.get("/:id", getCliente);
+router.put("/:id", updateCliente);
 
 export default router;
