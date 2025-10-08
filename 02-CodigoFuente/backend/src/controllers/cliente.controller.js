b6b76f32df1042be2a02a56939f89506b776ae20
Hide17298 <arnoldelgadocalle5@gmail.com>
2025-10-07 22:46:47 -0400

    refactor(RFC-002): refactorizacion de codigo a idioma español en controlador CLIENTE, PRODUCTO

--- 02-CodigoFuente/backend/src/controllers/cliente.controller.js
+++ 02-CodigoFuente/backend/src/controllers/cliente.controller.js
@@ -1,45 +1,77 @@
 import { Cliente } from "../models/Cliente.js";
-// import { Pedido } from "../models/Pedido.js";
-export const getClientes = async (req, res) => {
-    try {
-        const clientes = await Cliente.findAll();
+import { Pedido } from "../models/Pedido.js";
+
+export const listarClientes = async (req, res) => {
+    try{
+        const clientes = await Cliente.findAll()
         res.status(200).json(clientes);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
     }
-};
+    catch(error){
+        return res.status(500).json({message: error.message});
+   }   
+}
 
-export const createCliente = async (req, res) => {
-    try {
-        const { nombre, direccion, celular, email, estado } = req.body;
-        const cliente = await Cliente.create({ nombre, direccion, celular, email, estado });
-        res.status(201).json(cliente);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+export const crearCliente = async (req, res) => {
+    try{
+        const { nombre,
+            direccion,
+            celular,
+            email,
+            estado} = req.body
+    
+        const crearCliente = await Cliente.create({
+            nombre,
+            direccion,
+            celular,
+            email,
+            estado
+        });
+        res.status(201).json({
+            ok: true,
+            status: 201,
+            message: "Cliente Regitrado",
+        });
     }
-};
+    catch(error){
+        return res.status(500).json({message: error.message});
+   }
+}
 
-export const updateCliente = async (req, res) => {
-    try {
-        const cliente = await Cliente.findOne({ where: { id: req.params.id } });
+export const actualizarCliente = async (req, res) => {
+    const {id} = req.params
+    try{
+        const cliente = await Cliente.findOne({
+            where: {id},
+        });
         cliente.set(req.body);
         await cliente.save();
-        res.status(200).json(cliente);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+        
+        res.status(200).json({
+            message: "Registro Actualizado",
+            ok: true,
+            status: 200,
+            body: cliente,
+        });
+    }
+    catch(error){
+        return res.status(500).json({message: error.message});
     }
 };
 
-export const getCliente = async (req, res) => {
-    try {
-        const cliente = await Cliente.findOne({ where: { id: req.params.id } });
-        res.status(200).json(cliente);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
+export const verCliente = async (req, res) => {
+    const {id} = req.params
+       try{
+           const cliente = await Cliente.findOne({
+               where: {id},
+           });
+           res.status(200).json( cliente);
+       }
+       catch(error){
+            return res.status(500).json({message: error.message});
+       }
+}
 
-export const getClientePedidos = async (req, res) => {
+export const verClientePedidos = async (req, res) => {
     try {
         const pedidos = await Pedido.findAll({ where: { cliente_id: req.params.id } });
         res.status(200).json(pedidos);
@@ -47,6 +79,3 @@ export const getClientePedidos = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
-
-
-
