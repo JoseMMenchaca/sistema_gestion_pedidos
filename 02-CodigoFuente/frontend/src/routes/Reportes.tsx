b609a75e2cacc094543d80f5d9a9c5d0a228ea88
Hide17298 <arnoldelgadocalle5@gmail.com>
2025-10-10 19:42:51 -0400

    feat(rutas): Ajustes a la ruta de reportes

--- 02-CodigoFuente/frontend/src/routes/Reportes.tsx
+++ 02-CodigoFuente/frontend/src/routes/Reportes.tsx
@@ -1,8 +1,5 @@
 import HeaderAdmin from '../components/HeaderAdmin';
-import Hero from '../components/Hero';
-import FeaturedMenu from '../components/FeaturedMenu';
-import Footer from '../components/Footer';
-import ReportesVentas from '../components/ReporteProductosMasVendidos';
+import ReportesVentas from '../components/ReporteVentas';
 
 function Reportes() {
   return (
@@ -10,9 +7,7 @@ function Reportes() {
       <HeaderAdmin />
       <main>
 
-       <h1 className="text-3xl font-bold mb-6">Reportes de Ventas</h1>
-
-        {/* Aquí se renderiza el componente de reportes */}
+       <h1 className="text-3xl font-bold mb-6">...</h1>
         <ReportesVentas />
 
       </main>
