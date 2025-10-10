f335cf1a98d1b36c7d65ab1a5653d8df2853cfd0
Hide17298 <arnoldelgadocalle5@gmail.com>
2025-10-10 19:13:36 -0400

    feat(rutas): Implementacion de Reportes de Productos TOP

---
+++ 02-CodigoFuente/frontend/src/routes/RutaReporteProductos.tsx
@@ -0,0 +1,19 @@
+import HeaderAdmin from '../components/HeaderAdmin';
+import ReporteProductos from '../components/ReporteProductos';
+
+function RutaReporteProductos() {
+  return (
+    <>
+      <HeaderAdmin />
+      <main>
+
+       <h1 className="text-3xl font-bold mb-6">...</h1>
+        <ReporteProductos />
+
+      </main>
+
+    </>
+  );
+}
+
+export default RutaReporteProductos;
